974f421b4bd74a2431b1e61ca0f65884f14be422
Andrew Man <anman@ucar.edu>
2022-10-31 01:41:11 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,8 +2,42 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  const synth = window.speechSynthesis;
+// function init() {
+//   const voiceSelect = document.getElementById('voice-select');
+//   const synth = window.speechSynthesis;
+//   let voices = synth.getVoices();
+//   const selectedVoice = voices[0].name;
 
-  
-}
+//   for (let i = 0; i < voices.length ; i++) {
+//     const option = document.createElement('option');
+//     option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+//     // if (voices[i].default) {
+//     //   option.textContent += ' — DEFAULT';
+//     // }
+
+//     //option.setAttribute('data-lang', voices[i].lang);
+//     //option.setAttribute('data-name', voices[i].name);
+//     voiceSelect.appendChild(option);
+//   }
+
+//   voiceSelect.addEventListener('change', function(event) {
+//     selectedVoice = `${event.target.value}`;
+//     console.log(`${event.target.value}`);
+//   }) 
+
+//   const playBtn = document.getElementsByTagName("button");
+//   playBtn[0].addEventListener('click', function() {
+//     //if(audio != undefined) { 
+//       let userTxt = document.getElementById("text-to-speak").value;
+//       const utter = new SpeechSynthesisUtterance(userTxt.value);
+//       for (let i = 0; i < voices.length ; i++) {
+//         if (voices[i].name === selectedVoice) {
+//           utter.voice = voices[i];
+//         }
+//       }
+//       synth.speak(utter);
+//     //}
+//   }) 
+
+// }
